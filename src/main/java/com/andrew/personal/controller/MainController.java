eb9f651f8e6de3f632d27249995b073fb0cf39ee
Andrew <sjsh1623@naver.com>
2021-03-13 20:47:46 +0900

    Test my Mapping

--- src/main/java/com/andrew/personal/controller/MainController.java
+++ src/main/java/com/andrew/personal/controller/MainController.java
@@ -7,6 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
     @RequestMapping(value ="/")
     public String index() {
-        return "index.html";
+        return "index";
     }
+
+    @RequestMapping(value="/detail")
+    public String detail() {
+        return "detail";
+    }
+
+    @RequestMapping(value="/aboutMe")
+    public String aboutMe() {
+        return "text";
+    }
+
 }
