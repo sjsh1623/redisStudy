72e2c07c268e48dfff4a4c55bdcdf0874fa3cc14
임석현 <sjsh1623@flow.team>
2021-03-13 11:42:53 +0900

    HTML TEMPLATE UPDATE

---
+++ src/main/java/com/andrew/personal/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.andrew.personal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping(value ="/")
+    public String index() {
+        return "index.html";
+    }
+}
